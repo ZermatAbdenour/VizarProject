d1b5e348566573622d30caac010d826c1b958952
UnitDash <a_zermat@estin.dz>
2023-03-29 20:03:12 +0100

    Update

---
+++ app/src/main/java/com/example/vizar/ViewSellerAccount.java
@@ -0,0 +1,14 @@
+package com.example.vizar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewSellerAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_seller_account);
+    }
+}
---
+++ app/src/main/java/com/example/vizar/personal_account_view.java
@@ -0,0 +1,14 @@
+package com.example.vizar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class personal_account_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_account_view);
+    }
+}
